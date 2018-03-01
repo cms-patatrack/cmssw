86838e419f8880636fa22917044b92ac4d5a6ead
Andrea Bocci <fwyzard@gmail.com>
2018-03-01 20:32:48 +0100

    Use the `gpu` modifier to read the pixel clusters from the unpacker
    When running the GPU algorithms, the pixel unpacker is reponsible
    for providing both the digis and the cluster.
    These changes make use of the unpacker label to access the clusters,
    conditionally on the presence of the `gpu` process modifier.

--- RecoLocalTracker/SiPixelClusterizer/python/SiPixelClusterizerPreSplitting_cfi.py
+++ RecoLocalTracker/SiPixelClusterizer/python/SiPixelClusterizerPreSplitting_cfi.py
@@ -1,7 +1,9 @@
-
 import FWCore.ParameterSet.Config as cms
 
-#
 from CondTools.SiPixel.SiPixelGainCalibrationService_cfi import *
 from RecoLocalTracker.SiPixelClusterizer.SiPixelClusterizer_cfi import siPixelClusters as _siPixelClusters
 siPixelClustersPreSplitting = _siPixelClusters.clone()
+
+# *only for the cms-patatrack repository*
+# ensure reproducibility for CPU <--> GPU comparisons
+siPixelClustersPreSplitting.payloadType = 'HLT'
