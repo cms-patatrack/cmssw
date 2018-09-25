a96066857080595c73f61d60d4eb6615e9569808
Andrea Bocci <andrea.bocci@cern.ch>
2018-09-25 23:22:59 +0200

    check cudaGetLastError() after launching each kernel

--- RecoPixelVertexing/PixelVertexFinding/src/gpuVertexFinder.cu
+++ RecoPixelVertexing/PixelVertexFinding/src/gpuVertexFinder.cu
@@ -53,19 +53,18 @@ namespace gpuVertexFinder {
 			 uint32_t ntrks
 			 ) {
     
-
     cudaCheck(cudaMemcpyAsync(onGPU.zt,zt,ntrks*sizeof(float),
 			      cudaMemcpyHostToDevice,stream));
     cudaCheck(cudaMemcpyAsync(onGPU.ezt2,ezt2,ntrks*sizeof(float),
 			      cudaMemcpyHostToDevice,stream));
-
     cudaCheck(cudaMemcpyAsync(onGPU.ptt2,ptt2,ntrks*sizeof(float),
                               cudaMemcpyHostToDevice,stream));
     
-
     assert(onGPU_d);
     clusterTracks<<<1,1024-256,0,stream>>>(ntrks,onGPU_d,minT,eps,errmax,chi2max);
-    sortByPt2<<<1,256,0,stream>>>(ntrks,onGPU_d);    
+    cudaCheck(cudaGetLastError());
+    sortByPt2<<<1,256,0,stream>>>(ntrks,onGPU_d);
+    cudaCheck(cudaGetLastError());
 
     cudaCheck(cudaMemcpyAsync(&gpuProduct.nVertices, onGPU.nv, sizeof(uint32_t),
 			      cudaMemcpyDeviceToHost, stream));
@@ -73,8 +72,6 @@ namespace gpuVertexFinder {
     gpuProduct.ivtx.resize(ntrks);
     cudaCheck(cudaMemcpyAsync(gpuProduct.ivtx.data(),onGPU.iv,sizeof(int32_t)*ntrks,
 			      cudaMemcpyDeviceToHost, stream));
- 
-
   }
   
   Producer::GPUProduct const & Producer::fillResults(cudaStream_t stream) {
@@ -98,8 +95,5 @@ namespace gpuVertexFinder {
     
     return gpuProduct;
   }
-
-  
-  
+	
 } // end namespace
-
