1d5c42d15ba0a8808afa32c4a2b0d7267959d8e0
Viktor Khristenko <vdkhristenko1991@gmail.com>
2019-07-03 15:51:19 +0200

    producer and reco params conditions example

---
+++ RecoLocalCalo/HcalRecProducers/src/HBHERecHitProducerGPU.cc
@@ -0,0 +1,59 @@
+// framework
+#include "FWCore/Framework/interface/stream/EDProducer.h"
+//#include "HeterogeneousCore/Producer/interface/HeterogeneousEDProducer.h"
+//#include "HeterogeneousCore/Producer/interface/HeterogeneousEvent.h"
+
+#include "HeterogeneousCore/CUDAUtilities/interface/cudaCheck.h"
+#include "HeterogeneousCore/CUDACore/interface/CUDAScopedContext.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+class HBHERecHitProducerGPU : public edm::stream::EDProducer<edm::ExternalWork>
+{
+public:
+    explicit HBHERecHitProducerGPU(edm::ParameterSet const&);
+    ~HBHERecHitProducerGPU() override;
+    static void fillDescriptions(edm::ConfigurationDescriptions&);
+
+private:
+    void acquire(edm::Event const&,
+                 edm::EventSetup const&,
+                 edm::WaitingTaskWithArenaHolder) override;
+    void produce(edm::Event&, edm::EventSetup const&) override;
+
+    uint32_t maxChannels_;
+    CUDAContextState cudaState_;
+};
+
+HBHERecHitProducerGPU::HBHERecHitProducerGPU(edm::ParameterSet const& ps) 
+    : maxChannels_{ps.getParameter<uint32_t>("maxChannels")}
+{}
+
+HBHERecHitProducerGPU::~HBHERecHitProducerGPU() {}
+
+void HBHERecHitProducerGPU::fillDescriptions(edm::ConfigurationDescriptions& cdesc) {
+    edm::ParameterSetDescription desc;
+    desc.add<uint32_t>("maxChannels", 10000u);
+
+    std::string label = "hcalRecHitProducerGPU";
+    cdesc.add(label, desc);
+}
+
+void HBHERecHitProducerGPU::acquire(
+        edm::Event const& event,
+        edm::EventSetup const& setup,
+        edm::WaitingTaskWithArenaHolder holder) 
+{
+    CUDAScopedContextAcquire ctx{event.streamID(), std::move(holder), cudaState_};    
+}
+
+void HBHERecHitProducerGPU::produce(
+        edm::Event& event, 
+        edm::EventSetup const& setup) 
+{
+    CUDAScopedContextProduce ctx{cudaState_};
+}
+
+DEFINE_FWK_MODULE(HBHERecHitProducerGPU);
