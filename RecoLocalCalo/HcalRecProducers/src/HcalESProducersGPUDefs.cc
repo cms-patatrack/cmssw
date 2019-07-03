41e815092e10ae59eaeb6305e27a9eacc52d5026
Viktor Khristenko <vdkhristenko1991@gmail.com>
2019-07-03 17:52:32 +0200

    add gains, lut/resp/time corrections, pedestals

--- RecoLocalCalo/HcalRecProducers/src/HcalESProducersGPUDefs.cc
+++ RecoLocalCalo/HcalRecProducers/src/HcalESProducersGPUDefs.cc
@@ -1,13 +1,21 @@
 #include "HcalESProducerGPU.h"
 
 #include "CondFormats/HcalObjects/interface/HcalRecoParams.h"
+#include "CondFormats/HcalObjects/interface/HcalPedestals.h"
 #include "CondFormats/DataRecord/interface/HcalRecoParamsRcd.h"
+#include "CondFormats/DataRecord/interface/HcalPedestalsRcd.h"
 
 #include "RecoLocalCalo/HcalRecAlgos/interface/HcalRecoParamsGPU.h"
+#include "RecoLocalCalo/HcalRecAlgos/interface/HcalPedestalsGPU.h"
 
 #include <iostream>
 
 using HcalRecoParamsGPUESProducer = HcalESProducerGPU<HcalRecoParamsGPU,
                                                       HcalRecoParams,
                                                       HcalRecoParamsRcd>;
+using HcalPedestalsGPUESProducer = HcalESProducerGPU<HcalPedestalsGPU,
+                                                     HcalPedestals,
+                                                     HcalPedestalsRcd>;
+
 DEFINE_FWK_EVENTSETUP_MODULE(HcalRecoParamsGPUESProducer);
+DEFINE_FWK_EVENTSETUP_MODULE(HcalPedestalsGPUESProducer);
