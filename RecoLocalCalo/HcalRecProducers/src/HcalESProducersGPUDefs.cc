f05307bb7dc145cdfe903ba8b893038a571a9944
Viktor Khristenko <vdkhristenko1991@gmail.com>
2019-07-04 11:13:57 +0200

    gain and pedestal widths

--- RecoLocalCalo/HcalRecProducers/src/HcalESProducersGPUDefs.cc
+++ RecoLocalCalo/HcalRecProducers/src/HcalESProducersGPUDefs.cc
@@ -2,11 +2,30 @@
 
 #include "CondFormats/HcalObjects/interface/HcalRecoParams.h"
 #include "CondFormats/HcalObjects/interface/HcalPedestals.h"
+#include "CondFormats/HcalObjects/interface/HcalGains.h"
+#include "CondFormats/HcalObjects/interface/HcalLUTCorrs.h"
+#include "CondFormats/HcalObjects/interface/HcalRespCorrs.h"
+#include "CondFormats/HcalObjects/interface/HcalTimeCorrs.h"
+#include "CondFormats/HcalObjects/interface/HcalPedestalWidths.h"
+#include "CondFormats/HcalObjects/interface/HcalGainWidths.h"
+
 #include "CondFormats/DataRecord/interface/HcalRecoParamsRcd.h"
 #include "CondFormats/DataRecord/interface/HcalPedestalsRcd.h"
+#include "CondFormats/DataRecord/interface/HcalGainsRcd.h"
+#include "CondFormats/DataRecord/interface/HcalLUTCorrsRcd.h"
+#include "CondFormats/DataRecord/interface/HcalRespCorrsRcd.h"
+#include "CondFormats/DataRecord/interface/HcalTimeCorrsRcd.h"
+#include "CondFormats/DataRecord/interface/HcalPedestalWidthsRcd.h"
+#include "CondFormats/DataRecord/interface/HcalGainWidthsRcd.h"
 
 #include "RecoLocalCalo/HcalRecAlgos/interface/HcalRecoParamsGPU.h"
 #include "RecoLocalCalo/HcalRecAlgos/interface/HcalPedestalsGPU.h"
+#include "RecoLocalCalo/HcalRecAlgos/interface/HcalGainsGPU.h"
+#include "RecoLocalCalo/HcalRecAlgos/interface/HcalLUTCorrsGPU.h"
+#include "RecoLocalCalo/HcalRecAlgos/interface/HcalRespCorrsGPU.h"
+#include "RecoLocalCalo/HcalRecAlgos/interface/HcalTimeCorrsGPU.h"
+#include "RecoLocalCalo/HcalRecAlgos/interface/HcalPedestalWidthsGPU.h"
+#include "RecoLocalCalo/HcalRecAlgos/interface/HcalGainWidthsGPU.h"
 
 #include <iostream>
 
@@ -17,5 +36,35 @@ using HcalPedestalsGPUESProducer = HcalESProducerGPU<HcalPedestalsGPU,
                                                      HcalPedestals,
                                                      HcalPedestalsRcd>;
 
+using HcalGainsGPUESProducer = HcalESProducerGPU<HcalGainsGPU,
+                                                 HcalGains,
+                                                 HcalGainsRcd>;
+
+using HcalLUTCorrsGPUESProducer = HcalESProducerGPU<HcalLUTCorrsGPU,
+                                                     HcalLUTCorrs,
+                                                     HcalLUTCorrsRcd>;
+
+using HcalRespCorrsGPUESProducer = HcalESProducerGPU<HcalRespCorrsGPU,
+                                                     HcalRespCorrs,
+                                                     HcalRespCorrsRcd>;
+
+using HcalTimeCorrsGPUESProducer = HcalESProducerGPU<HcalTimeCorrsGPU,
+                                                     HcalTimeCorrs,
+                                                     HcalTimeCorrsRcd>;
+
+using HcalPedestalWidthsGPUESProducer = HcalESProducerGPU<HcalPedestalWidthsGPU,
+                                                     HcalPedestalWidths,
+                                                     HcalPedestalWidthsRcd>;
+
+using HcalGainWidthsGPUESProducer = HcalESProducerGPU<HcalGainWidthsGPU,
+                                                     HcalGainWidths,
+                                                     HcalGainWidthsRcd>;
+
 DEFINE_FWK_EVENTSETUP_MODULE(HcalRecoParamsGPUESProducer);
 DEFINE_FWK_EVENTSETUP_MODULE(HcalPedestalsGPUESProducer);
+DEFINE_FWK_EVENTSETUP_MODULE(HcalGainsGPUESProducer);
+DEFINE_FWK_EVENTSETUP_MODULE(HcalLUTCorrsGPUESProducer);
+DEFINE_FWK_EVENTSETUP_MODULE(HcalRespCorrsGPUESProducer);
+DEFINE_FWK_EVENTSETUP_MODULE(HcalTimeCorrsGPUESProducer);
+DEFINE_FWK_EVENTSETUP_MODULE(HcalPedestalWidthsGPUESProducer);
+DEFINE_FWK_EVENTSETUP_MODULE(HcalGainWidthsGPUESProducer);
