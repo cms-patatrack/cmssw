f8c17b89374600445bef7c99fab3407cc3852b8e
Andrea Bocci <andrea.bocci@cern.ch>
2019-12-11 13:51:01 +0100

    Solution to the fourth exercise

---
+++ Patatrack/Tutorial/test/printCartesianVectorsAuto.py
@@ -0,0 +1,37 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("PRINT")
+
+process.options = cms.untracked.PSet(
+  numberOfThreads = cms.untracked.uint32( 1 ),
+  numberOfStreams = cms.untracked.uint32( 1 ),
+  wantSummary = cms.untracked.bool( False )
+)
+
+process.load("FWCore.MessageService.MessageLogger_cfi")
+process.load("HeterogeneousCore.CUDAServices.CUDAService_cfi")
+process.MessageLogger.categories.append("CUDAService")
+
+process.source = cms.Source("PoolSource",
+  fileNames = cms.untracked.vstring("file:cylindricalVectors.root"),
+)
+
+from HeterogeneousCore.CUDACore.SwitchProducerCUDA import SwitchProducerCUDA
+process.convertToCartesianVectors = SwitchProducerCUDA(
+  cpu = cms.EDProducer('ConvertToCartesianVectors',
+    input = cms.InputTag('generateCylindricalVectors')
+  ),
+  cuda = cms.EDProducer('ConvertToCartesianVectorsCUDA',
+    input = cms.InputTag('generateCylindricalVectors')
+  )
+)
+
+process.printCartesianVectors = cms.EDAnalyzer('PrintCartesianVectors',
+  input = cms.InputTag('convertToCartesianVectors')
+)
+
+process.path = cms.Path(process.convertToCartesianVectors + process.printCartesianVectors)
+
+process.maxEvents = cms.untracked.PSet(
+  input = cms.untracked.int32( 1 )
+)
