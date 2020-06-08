907a3bb265c30d332432084a72b1748f2b51971c
amassiro <massironi.andrea@gmail.com>
2020-06-08 15:04:15 +0200

    ops, a cout slipped through

--- RecoLocalCalo/EcalRecProducers/plugins/EcalRecHitProducerGPU.cc
+++ RecoLocalCalo/EcalRecProducers/plugins/EcalRecHitProducerGPU.cc
@@ -302,7 +302,7 @@ void EcalRecHitProducerGPU::acquire(edm::Event const& event,
 
   neb_ = ebUncalibRecHits.size;
   nee_ = eeUncalibRecHits.size;
-  std::cout << " [EcalRecHitProducerGPU::acquire]  neb_:nee_ = " << neb_ << " : " << nee_ << std::endl;
+  // std::cout << " [EcalRecHitProducerGPU::acquire]  neb_:nee_ = " << neb_ << " : " << nee_ << std::endl;
 
   int nchannelsEB = ebUncalibRecHits.size;  // --> offsetForInput, first EB and then EE
 
