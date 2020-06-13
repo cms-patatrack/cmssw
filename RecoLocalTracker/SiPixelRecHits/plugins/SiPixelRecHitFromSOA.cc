28c5e7099746364c00dd03cff84dc973b712bca5
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2020-06-13 18:42:55 +0200

    cluserless

--- RecoLocalTracker/SiPixelRecHits/plugins/SiPixelRecHitFromSOA.cc
+++ RecoLocalTracker/SiPixelRecHits/plugins/SiPixelRecHitFromSOA.cc
@@ -30,6 +30,7 @@ public:
   static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
 
   using HMSstorage = HostProduct<unsigned int[]>;
+  using HLPstorage = HostProduct<float[]>;
 
 private:
   void acquire(edm::Event const& iEvent,
@@ -44,20 +45,27 @@ private:
   cms::cuda::host::unique_ptr<uint16_t[]> m_store16;
   cms::cuda::host::unique_ptr<float[]> m_store32;
   cms::cuda::host::unique_ptr<uint32_t[]> m_hitsModuleStart;
+
+  bool m_clusterLess;
+
 };
 
 SiPixelRecHitFromSOA::SiPixelRecHitFromSOA(const edm::ParameterSet& iConfig)
     : tokenHit_(
           consumes<cms::cuda::Product<TrackingRecHit2DCUDA>>(iConfig.getParameter<edm::InputTag>("pixelRecHitSrc"))),
-      clusterToken_(consumes<SiPixelClusterCollectionNew>(iConfig.getParameter<edm::InputTag>("src"))) {
+      m_clusterLess(iConfig.getParameter<bool>("cluserLess")) {
+  if (m_clusterLess)
+     clusterToken_ = consumes<SiPixelClusterCollectionNew>(iConfig.getParameter<edm::InputTag>("src"));
   produces<SiPixelRecHitCollectionNew>();
   produces<HMSstorage>();
+  produces<HLPstorage>();
 }
 
 void SiPixelRecHitFromSOA::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
   edm::ParameterSetDescription desc;
   desc.add<edm::InputTag>("pixelRecHitSrc", edm::InputTag("siPixelRecHitsCUDAPreSplitting"));
   desc.add<edm::InputTag>("src", edm::InputTag("siPixelClustersPreSplitting"));
+   desc.add<bool>("cluserLess",false);
   descriptions.add("siPixelRecHitFromSOA", desc);
 }
 
@@ -81,11 +89,14 @@ void SiPixelRecHitFromSOA::acquire(edm::Event const& iEvent,
 
 void SiPixelRecHitFromSOA::produce(edm::Event& iEvent, edm::EventSetup const& es) {
   // yes a unique ptr of a unique ptr so edm is happy
+
+  constexpr uint32_t MaxHitsInModule = gpuClustering::MaxHitsInModule;
   auto sizeOfHitModuleStart = gpuClustering::MaxNumModules + 1;
   auto hmsp = std::make_unique<uint32_t[]>(sizeOfHitModuleStart);
   std::copy(m_hitsModuleStart.get(), m_hitsModuleStart.get() + sizeOfHitModuleStart, hmsp.get());
   auto hms = std::make_unique<HMSstorage>(std::move(hmsp));  // hmsp is gone
   iEvent.put(std::move(hms));                                // hms is gone!
+
 
   auto output = std::make_unique<SiPixelRecHitCollectionNew>();
   if (0 == m_nHits) {
@@ -98,16 +109,92 @@ void SiPixelRecHitFromSOA::produce(edm::Event& iEvent, edm::EventSetup const& es
   auto xe = yl + m_nHits;
   auto ye = xe + m_nHits;
 
-  edm::ESHandle<TrackerGeometry> geom;
-  es.get<TrackerDigiGeometryRecord>().get(geom);
-  geom = geom.product();
+  auto hlp = std::make_unique<HLPstorage>(std::move(m_store32)); // m_store32 is gone!
+
+
+  edm::ESHandle<TrackerGeometry> hgeom;
+  es.get<TrackerDigiGeometryRecord>().get(hgeom);
+  auto const & geom = *hgeom.product();
+
+
+  if (m_clusterLess) {
+
+  // the usual mess
+  auto const& dus = geom.detUnits();
+  unsigned m_detectors = dus.size();
+  for (unsigned int i = 1; i < 7; ++i) {
+    LogDebug("PixelCPEBase:: LookingForFirstStrip")
+        << "Subdetector " << i << " GeomDetEnumerator " << GeomDetEnumerators::tkDetEnum[i] << " offset "
+        << geom.offsetDU(GeomDetEnumerators::tkDetEnum[i]) << " is it strip? "
+        << (geom.offsetDU(GeomDetEnumerators::tkDetEnum[i]) != dus.size()
+                ? dus[geom.offsetDU(GeomDetEnumerators::tkDetEnum[i])]->type().isOuterTracker()
+                : false);
+
+    if (geom.offsetDU(GeomDetEnumerators::tkDetEnum[i]) != dus.size() &&
+        dus[geom.offsetDU(GeomDetEnumerators::tkDetEnum[i])]->type().isOuterTracker()) {
+      if (geom.offsetDU(GeomDetEnumerators::tkDetEnum[i]) < m_detectors) {
+        m_detectors = geom.offsetDU(GeomDetEnumerators::tkDetEnum[i]);
+      }
+    }
+  }
+
+  assert (m_detectors<=gpuClustering::MaxNumModules);
+  for (int gind = 0; gind < int(m_detectors); ++gind) {
+    
+
+    const PixelGeomDetUnit* pixDet = dynamic_cast<const PixelGeomDetUnit*>(dus[gind]);
+    assert(pixDet);
+    assert(pixDet->index() == gind);
+    auto detid = pixDet->geographicalId();
+    SiPixelRecHitCollectionNew::FastFiller recHitsOnDetUnit(*output, detid);
+    int fc = m_hitsModuleStart[gind];
+    int lc = m_hitsModuleStart[gind + 1];
+    int nhits = lc - fc;
+    if (0 == nhits)
+      continue;
+
+    assert(lc > fc);
+    // std::cout << "in det " << gind << ": conv " << nhits << " hits " << fc <<','<<lc<<std::endl;
+    if (nhits > int(MaxHitsInModule))
+      printf(
+          "WARNING: too many clusters %d in Module %d. Only first %d Hits converted\n", nhits, gind, MaxHitsInModule);
+    nhits = std::min(nhits, int(MaxHitsInModule));
+    auto jnd = [&](int k) { return fc + k; };
+    for (int i=0; i<nhits; ++i) {
+      auto ij = jnd(i);
+      if (ij >= int(TrackingRecHit2DSOAView::maxHits()))
+        continue;  // overflow...
+      LocalPoint lp(xl[ij], yl[ij]);
+      LocalError le(xe[ij], 0, ye[ij]);
+      SiPixelRecHitQuality::QualWordType rqw = 0;
+
+      // Create a persistent edm::Ref to the cluster
+      edm::Ref<edmNew::DetSetVector<SiPixelCluster>, SiPixelCluster> cluster; //  = edmNew::makeRefTo(hclusters, &clust);
+      // Make a RecHit and add it to the DetSet
+      SiPixelRecHit hit(lp, le, rqw, *pixDet, cluster);
+      //
+      // Now save it =================
+      recHitsOnDetUnit.push_back(hit);
+      std::push_heap(recHitsOnDetUnit.begin(), recHitsOnDetUnit.end(), [](auto const& h1, auto const& h2) {
+        return h1.localPosition().x() < h2.localPosition().x();
+      });
+      // =============================
+
+
+    }  // hits
+    std::sort_heap(recHitsOnDetUnit.begin(), recHitsOnDetUnit.end(), [](auto const& h1, auto const& h2) {
+           return h1.localPosition().x() < h2.localPosition().x();
+      });
+
+
+
+  } // detunit
+  } else {
 
   edm::Handle<SiPixelClusterCollectionNew> hclusters;
   iEvent.getByToken(clusterToken_, hclusters);
 
   auto const& input = *hclusters;
-
-  constexpr uint32_t MaxHitsInModule = gpuClustering::MaxHitsInModule;
 
   int numberOfDetUnits = 0;
   int numberOfClusters = 0;
@@ -115,7 +202,7 @@ void SiPixelRecHitFromSOA::produce(edm::Event& iEvent, edm::EventSetup const& es
     numberOfDetUnits++;
     unsigned int detid = DSViter->detId();
     DetId detIdObject(detid);
-    const GeomDetUnit* genericDet = geom->idToDetUnit(detIdObject);
+    const GeomDetUnit* genericDet = geom.idToDetUnit(detIdObject);
     auto gind = genericDet->index();
     const PixelGeomDetUnit* pixDet = dynamic_cast<const PixelGeomDetUnit*>(genericDet);
     assert(pixDet);
@@ -190,7 +277,9 @@ void SiPixelRecHitFromSOA::produce(edm::Event& iEvent, edm::EventSetup const& es
     << std::endl;
   */
 
+  }
   iEvent.put(std::move(output));
+  iEvent.put(std::move(hlp));
 }
 
 DEFINE_FWK_MODULE(SiPixelRecHitFromSOA);
