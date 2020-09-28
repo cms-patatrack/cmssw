8fa065e4e42952c4a2d1bf97ff91ae7a5f33f1bb
adriano <adriano.di.florio@cern.ch>
2020-09-28 19:18:16 +0200

    Moving from load to import+clone

--- RecoPixelVertexing/Configuration/python/customizePixelTracksSoAonCPU.py
+++ RecoPixelVertexing/Configuration/python/customizePixelTracksSoAonCPU.py
@@ -3,20 +3,25 @@ import FWCore.ParameterSet.Config as cms
 def customizePixelTracksSoAonCPU(process) :
 
   process.load('RecoLocalTracker/SiPixelRecHits/siPixelRecHitHostSoA_cfi')
-  process.load('RecoPixelVertexing.PixelTriplets.caHitNtupletCUDA_cfi')
-  process.load('RecoPixelVertexing.PixelVertexFinding.pixelVertexCUDA_cfi')
+  
+  from RecoPixelVertexing.PixelTriplets.caHitNtupletCUDA_cfi import *
+  from RecoPixelVertexing.PixelVertexFinding.pixelVertexCUDA_cfi import *
+ 
+  from RecoPixelVertexing.PixelVertexFinding.pixelVertexFromSoA_cfi import *
+  from RecoPixelVertexing.PixelTrackFitting.pixelTrackProducerFromSoA_cfi import *
+  
+  process.CUDAService = cms.Service("CUDAService")
+  process.CUDAService.enabled = cms.untracked.bool(False)
 
-  process.pixelTrackSoA = process.caHitNtupletCUDA.clone()
+  process.pixelTrackSoA = caHitNtupletCUDA.clone()
   process.pixelTrackSoA.onGPU = False
   process.pixelTrackSoA.pixelRecHitSrc = 'siPixelRecHitHostSoA'
-  process.pixelVertexSoA = process.pixelVertexCUDA.clone()
+  process.pixelVertexSoA = pixelVertexCUDA.clone()
   process.pixelVertexSoA.onGPU = False
-  process.pixelVertexSoA.pixelTrackSrc = 'pixelTrackSoA'
+  process.pixelVertexSoA.pixelTrackSrc = 'pixelTrackSoA' 
 
-  process.load('RecoPixelVertexing.PixelTrackFitting.pixelTrackProducerFromSoA_cfi')
-  process.pixelTracks = process.pixelTrackProducerFromSoA.clone()
-  process.load('RecoPixelVertexing.PixelVertexFinding.pixelVertexFromSoA_cfi')
-  process.pixelVertices = process.pixelVertexFromSoA.clone()
+  process.pixelTracks = pixelTrackProducerFromSoA.clone()
+  process.pixelVertices = pixelVertexFromSoA.clone()
   process.pixelTracks.pixelRecHitLegacySrc = 'siPixelRecHitHostSoA'
   process.siPixelRecHitHostSoA.convertToLegacy = True
 
@@ -26,27 +31,28 @@ def customizePixelTracksSoAonCPU(process) :
 
 def customizePixelTracksSoAonCPUForWF(process) :
 
-  process.load('RecoLocalTracker/SiPixelRecHits/siPixelRecHitHostSoA_cfi')
-  process.load('RecoPixelVertexing.PixelTriplets.caHitNtupletCUDA_cfi')
-  process.load('RecoPixelVertexing.PixelVertexFinding.pixelVertexCUDA_cfi')
-  
+  from RecoPixelVertexing.PixelTriplets.caHitNtupletCUDA_cfi import *
+  from RecoPixelVertexing.PixelVertexFinding.pixelVertexCUDA_cfi import *
+  from RecoLocalTracker.SiPixelRecHits.siPixelRecHitHostSoA_cfi import *
+  from RecoPixelVertexing.PixelVertexFinding.pixelVertexFromSoA_cfi import *
+  from RecoPixelVertexing.PixelTrackFitting.pixelTrackProducerFromSoA_cfi import *
+
   process.CUDAService = cms.Service("CUDAService")
-  process.CUDAService. enabled = cms.untracked.bool(False)
+  process.CUDAService.enabled = cms.untracked.bool(False)
   
-  process.pixelTrackSoA = process.caHitNtupletCUDA.clone()
+  process.pixelTrackSoA = caHitNtupletCUDA.clone()
   process.pixelTrackSoA.onGPU = False
   process.pixelTrackSoA.pixelRecHitSrc = 'siPixelRecHitsPreSplitting'
-  process.pixelVertexSoA = process.pixelVertexCUDA.clone()
+  process.pixelVertexSoA = pixelVertexCUDA.clone()
   process.pixelVertexSoA.onGPU = False
   process.pixelVertexSoA.pixelTrackSrc = 'pixelTrackSoA'
 
-  process.load('RecoPixelVertexing.PixelTrackFitting.pixelTrackProducerFromSoA_cfi')
-  process.pixelTracks = process.pixelTrackProducerFromSoA.clone()
-  process.load('RecoPixelVertexing.PixelVertexFinding.pixelVertexFromSoA_cfi')
-  process.pixelVertices = process.pixelVertexFromSoA.clone()
+  process.pixelTracks = pixelTrackProducerFromSoA.clone()
+
+  process.pixelVertices = pixelVertexFromSoA.clone()
   process.pixelTracks.pixelRecHitLegacySrc = 'siPixelRecHitsPreSplitting'
 
-  process.siPixelRecHitsPreSplitting = process.siPixelRecHitHostSoA.clone()
+  process.siPixelRecHitsPreSplitting = siPixelRecHitHostSoA.clone()
   process.siPixelRecHitsPreSplitting.convertToLegacy = True
 
   process.reconstruction_step += process.pixelTrackSoA+process.pixelVertexSoA
