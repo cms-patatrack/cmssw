46093b70185682086e22851fff223d7f65e0d770
Andrea Bocci <andrea.bocci@cern.ch>
2020-10-01 15:47:12 +0200

    Merge into a single customizePixelTracksSoAonCPU customisation

--- RecoPixelVertexing/Configuration/python/customizePixelTracksSoAonCPU.py
+++ RecoPixelVertexing/Configuration/python/customizePixelTracksSoAonCPU.py
@@ -1,75 +1,54 @@
 import FWCore.ParameterSet.Config as cms
 
 def customizePixelTracksSoAonCPU(process) :
-
-  process.load('RecoLocalTracker/SiPixelRecHits/siPixelRecHitHostSoA_cfi')
   
-  from RecoPixelVertexing.PixelTriplets.caHitNtupletCUDA_cfi import *
-  from RecoPixelVertexing.PixelVertexFinding.pixelVertexCUDA_cfi import *
- 
-  from RecoPixelVertexing.PixelVertexFinding.pixelVertexFromSoA_cfi import *
-  from RecoPixelVertexing.PixelTrackFitting.pixelTrackProducerFromSoA_cfi import *
-  
-  process.CUDAService = cms.Service("CUDAService")
-  process.CUDAService.enabled = cms.untracked.bool(False)
+  process.CUDAService = cms.Service("CUDAService",
+    enabled = cms.untracked.bool(False)
+  )
 
-  process.pixelTrackSoA = caHitNtupletCUDA.clone()
-  process.pixelTrackSoA.onGPU = False
-  process.pixelTrackSoA.pixelRecHitSrc = 'siPixelRecHitHostSoA'
-  process.pixelVertexSoA = pixelVertexCUDA.clone()
-  process.pixelVertexSoA.onGPU = False
-  process.pixelVertexSoA.pixelTrackSrc = 'pixelTrackSoA'
-
-  process.pixelTracks = pixelTrackProducerFromSoA.clone()
-  process.pixelVertices = pixelVertexFromSoA.clone()
-  process.pixelTracks.pixelRecHitLegacySrc = 'siPixelRecHitHostSoA'
-  process.siPixelRecHitHostSoA.convertToLegacy = True
-
-  process.reconstruction_step += process.siPixelRecHitHostSoA+process.pixelTrackSoA+process.pixelVertexSoA
-
-  return process
-
-def customizePixelTracksSoAonCPUForWF(process) :
-
-  from RecoPixelVertexing.PixelTriplets.caHitNtupletCUDA_cfi import *
-  from RecoPixelVertexing.PixelVertexFinding.pixelVertexCUDA_cfi import *
   from RecoLocalTracker.SiPixelRecHits.siPixelRecHitHostSoA_cfi import *
-  from RecoPixelVertexing.PixelVertexFinding.pixelVertexFromSoA_cfi import *
+  process.siPixelRecHitsPreSplitting = siPixelRecHitHostSoA.clone(
+    convertToLegacy = True
+  )
+
+  from RecoPixelVertexing.PixelTriplets.caHitNtupletCUDA_cfi import *
+  process.pixelTrackSoA = caHitNtupletCUDA.clone(
+    onGPU = False,
+    pixelRecHitSrc = 'siPixelRecHitsPreSplitting'
+  )
+
+  from RecoPixelVertexing.PixelVertexFinding.pixelVertexCUDA_cfi import *
+  process.pixelVertexSoA = pixelVertexCUDA.clone(
+    onGPU = False,
+    pixelTrackSrc = 'pixelTrackSoA'
+  )
+
   from RecoPixelVertexing.PixelTrackFitting.pixelTrackProducerFromSoA_cfi import *
+  process.pixelTracks = pixelTrackProducerFromSoA.clone(
+    pixelRecHitLegacySrc = 'siPixelRecHitsPreSplitting'
+  )
 
-  process.CUDAService = cms.Service("CUDAService")
-  process.CUDAService.enabled = cms.untracked.bool(False)
-  
-  process.pixelTrackSoA = caHitNtupletCUDA.clone()
-  process.pixelTrackSoA.onGPU = False
-  process.pixelTrackSoA.pixelRecHitSrc = 'siPixelRecHitsPreSplitting'
-  process.pixelVertexSoA = pixelVertexCUDA.clone()
-  process.pixelVertexSoA.onGPU = False
-  process.pixelVertexSoA.pixelTrackSrc = 'pixelTrackSoA'
-
-  process.pixelTracks = pixelTrackProducerFromSoA.clone()
-
+  from RecoPixelVertexing.PixelVertexFinding.pixelVertexFromSoA_cfi import *
   process.pixelVertices = pixelVertexFromSoA.clone()
-  process.pixelTracks.pixelRecHitLegacySrc = 'siPixelRecHitsPreSplitting'
 
-  process.siPixelRecHitsPreSplitting = siPixelRecHitHostSoA.clone()
-  process.siPixelRecHitsPreSplitting.convertToLegacy = True
-
-  process.reconstruction_step += process.pixelTrackSoA+process.pixelVertexSoA
+  process.reconstruction_step += process.siPixelRecHitsPreSplitting + process.pixelTrackSoA + process.pixelVertexSoA
 
   return process
+
 
 def customizePixelTracksForTriplets(process) :
  
   if 'caHitNtupletCUDA' in process.__dict__:
         process.caHitNtupletCUDA.includeJumpingForwardDoublets = True
         process.caHitNtupletCUDA.minHitsPerNtuplet = 3
+
   if 'pixelTrackSoA' in process.__dict__:
         process.pixelTrackSoA.includeJumpingForwardDoublets = True
         process.pixelTrackSoA.minHitsPerNtuplet = 3
  
   return process
  
+
 def customizePixelTracksSoAonCPUForProfiling(process) :
 
   process.MessageLogger.cerr.FwkReport.reportEvery = 100
