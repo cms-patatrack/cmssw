5e09bb0b06c67eb0dd7ef60288a8db53f01979bb
Thomas Reis <thomas.reis@cern.ch>
2020-12-11 11:26:02 +0100

    Remove unused struct and includes.

--- RecoLocalCalo/EcalRecProducers/plugins/Common.h
+++ RecoLocalCalo/EcalRecProducers/plugins/Common.h
@@ -1,14 +1,7 @@
 #ifndef RecoLocalCalo_EcalRecProducers_plugins_Common_h
 #define RecoLocalCalo_EcalRecProducers_plugins_Common_h
 
-#include <cassert>
-#include <chrono>
-#include <cmath>
-#include <cstdint>
-#include <iostream>
-
-#include <cuda.h>
-#include <cuda_runtime.h>
+#include <algorithm>
 
 // a workaround for std::abs not being a constexpr function
 namespace ecal {
@@ -27,20 +20,5 @@ namespace ecal {
   }  // namespace mgpa
 
 }  // namespace ecal
-
-template <typename T>
-struct DurationMeasurer {
-  DurationMeasurer(std::string const& msg) : msg_{msg}, start_{std::chrono::high_resolution_clock::now()} {}
-
-  ~DurationMeasurer() {
-    auto end = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<T>(end - start_).count();
-    std::cout << msg_ << "\nduration = " << duration << std::endl;
-  }
-
-private:
-  std::string msg_;
-  std::chrono::high_resolution_clock::time_point start_;
-};
 
 #endif  // RecoLocalCalo_EcalRecProducers_plugins_Common_h
