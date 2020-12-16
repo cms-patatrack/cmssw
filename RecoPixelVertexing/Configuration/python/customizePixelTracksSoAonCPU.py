68db8f8f3d97753edd38a3ba691e102ec86a82c9
Andrea Bocci <andrea.bocci@cern.ch>
2020-12-16 15:40:22 +0100

    Backport Patatrack customisations from 11.2.x

--- RecoPixelVertexing/Configuration/python/customizePixelTracksSoAonCPU.py
+++ RecoPixelVertexing/Configuration/python/customizePixelTracksSoAonCPU.py
@@ -1,36 +1,61 @@
 import FWCore.ParameterSet.Config as cms
 
-def customizePixelTracksSoAonCPU(process) :
+def customizePixelTracksSoAonCPU(process):
 
-  process.load('RecoLocalTracker/SiPixelRecHits/siPixelRecHitHostSoA_cfi')
-  process.load('RecoPixelVertexing.PixelTriplets.caHitNtupletCUDA_cfi')
-  process.load('RecoPixelVertexing.PixelVertexFinding.pixelVertexCUDA_cfi')
+  process.CUDAService = cms.Service('CUDAService',
+    enabled = cms.untracked.bool(False)
+  )
 
-  process.pixelTrackSoA = process.caHitNtupletCUDA.clone()
-  process.pixelTrackSoA.onGPU = False
-  process.pixelTrackSoA.pixelRecHitSrc = 'siPixelRecHitHostSoA'
-  process.pixelVertexSoA = process.pixelVertexCUDA.clone()
-  process.pixelVertexSoA.onGPU = False
-  process.pixelVertexSoA.pixelTrackSrc = 'pixelTrackSoA'
+  # ensure the same results when running on GPU (which supports only the 'HLT' payload) and CPU
+  process.siPixelClustersPreSplitting.cpu.payloadType = cms.string('HLT')
 
-  process.load('RecoPixelVertexing.PixelTrackFitting.pixelTrackProducerFromSoA_cfi')
-  process.pixelTracks = process.pixelTrackProducerFromSoA.clone()
-  process.load('RecoPixelVertexing.PixelVertexFinding.pixelVertexFromSoA_cfi')
-  process.pixelVertices = process.pixelVertexFromSoA.clone()
-  process.pixelTracks.pixelRecHitLegacySrc = 'siPixelRecHitHostSoA'
-  process.siPixelRecHitHostSoA.convertToLegacy = True
+  from RecoLocalTracker.SiPixelRecHits.siPixelRecHitHostSoA_cfi import siPixelRecHitHostSoA
+  process.siPixelRecHitsPreSplitting = siPixelRecHitHostSoA.clone(
+    convertToLegacy = True
+  )
 
-  process.reconstruction_step += process.siPixelRecHitHostSoA+process.pixelTrackSoA+process.pixelVertexSoA
+  from RecoPixelVertexing.PixelTriplets.caHitNtupletCUDA_cfi import caHitNtupletCUDA
+  process.pixelTrackSoA = caHitNtupletCUDA.clone(
+    onGPU = False,
+    pixelRecHitSrc = 'siPixelRecHitsPreSplitting'
+  )
+
+  from RecoPixelVertexing.PixelVertexFinding.pixelVertexCUDA_cfi import pixelVertexCUDA
+  process.pixelVertexSoA = pixelVertexCUDA.clone(
+    onGPU = False,
+    pixelTrackSrc = 'pixelTrackSoA'
+  )
+
+  from RecoPixelVertexing.PixelTrackFitting.pixelTrackProducerFromSoA_cfi import pixelTrackProducerFromSoA
+  process.pixelTracks = pixelTrackProducerFromSoA.clone(
+    pixelRecHitLegacySrc = 'siPixelRecHitsPreSplitting'
+  )
+
+  from RecoPixelVertexing.PixelVertexFinding.pixelVertexFromSoA_cfi import pixelVertexFromSoA
+  process.pixelVertices = pixelVertexFromSoA.clone()
+
+  process.reconstruction_step += process.siPixelRecHitsPreSplitting + process.pixelTrackSoA + process.pixelVertexSoA
 
   return process
 
-def customizePixelTracksSoAonCPUForProfiling(process) :
+
+def customizePixelTracksForTriplets(process):
+
+  from HLTrigger.Configuration.common import producers_by_type
+  for producer in producers_by_type(process, 'CAHitNtupletCUDA'):
+        producer.includeJumpingForwardDoublets = True
+        producer.minHitsPerNtuplet = 3
+
+  return process
+
+
+def customizePixelTracksSoAonCPUForProfiling(process):
 
   process.MessageLogger.cerr.FwkReport.reportEvery = 100
 
   process = customizePixelTracksSoAonCPU(process)
   process.siPixelRecHitHostSoA.convertToLegacy = False
-  
+
   process.TkSoA = cms.Path(process.offlineBeamSpot+process.siPixelDigis+process.siPixelClustersPreSplitting+process.siPixelRecHitHostSoA+process.pixelTrackSoA+process.pixelVertexSoA)
   process.schedule = cms.Schedule(process.TkSoA)
   return process
